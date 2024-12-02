404690701d3427f86c66153990d27d947295dd39
manueljo <mikebillyy@gmail.com>
2024-12-02 17:39:44 +0100

    onboarding screens complete

--- app/index.jsx
+++ app/index.jsx
@@ -1,12 +1,9 @@
+import { Redirect } from 'expo-router'
 import { View, Text } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 
 const Home = () => {
-  return (
-    <SafeAreaView>
-      <Text>Home</Text>
-    </SafeAreaView>
-  )
+  return <Redirect href="/(auth)/welcome" />
 }
 
 export default Home
